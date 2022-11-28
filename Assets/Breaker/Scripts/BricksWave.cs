6b56da2d1d0a0e0222fdae3afb5f2d434a227e58
Kirill Larionov <strrranger1@gmail.com>
2022-11-29 00:49:50 +0300

    Yandex Web Start

---
+++ Assets/Breaker/Scripts/BricksWave.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BricksWave : MonoBehaviour {
+
+	private int wave = 1;
+	private Rigidbody2D rb;
+	private TextMeshPro waveText;
+	private AudioSource brickHitSound;
+	public Renderer brick;
+	void Start() {
+		brickHitSound = GameObject.Find ("brickHitSound").GetComponent<AudioSource> ();
+		rb = GetComponent<Rigidbody2D> ();
+		if (transform.Find ("brickWaveText") != null) {
+			waveText = transform.Find ("brickWaveText").GetComponent<TextMeshPro> ();
+
+			if (Vars.level < 10) {
+				wave = Random.Range (1, 3);
+			} else {
+				wave = Random.Range ((int)(Vars.level / 5), (int)(Vars.level / 2));
+			}
+			waveText.text = wave.ToString ();
+		}
+
+
+		if(this.gameObject.name.Contains("brick")) {
+			ColorBrick ();
+		}
+	}
+		
+	void OnCollisionEnter2D(Collision2D col) {
+		if (!brickHitSound.isPlaying) {
+			brickHitSound.Play ();
+		}
+		wave--;
+		ColorBrick ();
+		waveText.text = wave.ToString();
+		if (wave <= 0) {
+
+			PlayerPrefs.SetInt ("numberOfBricksDestroyed", PlayerPrefs.GetInt ("numberOfBricksDestroyed") + 1);
+			if (PlayerPrefs.GetInt ("numberOfBricksDestroyed") >= 100) {
+				if (PlayerPrefs.GetInt ("destroy100bricks") != 1) {
+					PlayerPrefs.SetInt ("destroy100bricks", 1);
+					GameObject.Find ("Canvas").GetComponent<AchievementUnlocked> ().enabled = true;
+					GameObject.Find ("Canvas").GetComponent<AchievementUnlocked> ().NameOfTheAchievement ("destroy 100 bricks");
+				}
+			}
+
+			if (PlayerPrefs.GetInt ("numberOfBricksDestroyed") >= 1000) {
+				if (PlayerPrefs.GetInt ("destroy1000bricks") != 1) {
+					PlayerPrefs.SetInt ("destroy1000bricks", 1);
+					GameObject.Find ("Canvas").GetComponent<AchievementUnlocked> ().enabled = true;
+					GameObject.Find ("Canvas").GetComponent<AchievementUnlocked> ().NameOfTheAchievement ("destroy 1000 bricks");
+				}
+			}
+
+			if (PlayerPrefs.GetInt ("numberOfBricksDestroyed") >= 10000) {
+				if (PlayerPrefs.GetInt ("destroy10000bricks") != 1) {
+					PlayerPrefs.SetInt ("destroy10000bricks", 1);
+					GameObject.Find ("Canvas").GetComponent<AchievementUnlocked> ().enabled = true;
+					GameObject.Find ("Canvas").GetComponent<AchievementUnlocked> ().NameOfTheAchievement ("destroy 10000 bricks");
+				}
+			}
+
+			Destroy (this.gameObject);
+		}
+	}
+
+	public void ColorBrick() {
+		if (wave <= 30) {
+			brick.material.SetColor("_Color", new Color (1, (1 - ((float)wave / 30)), 0));
+		} else if (wave <= 60) {
+			brick.material.SetColor("_Color", new Color (1, 0, (float)(wave - 30) / 30));
+		} else {
+			float redColorValue = 1-(((float)wave - 60) / 30);
+			if (redColorValue < 1) {
+				brick.material.SetColor("_Color", new Color (redColorValue, 0, 1));			
+			} else {
+				brick.material.SetColor("_Color", new Color (1, 0, 1));	
+			}
+		}
+	}
+}
---
+++ Assets/Breaker/Scripts/CameraSize.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSize : MonoBehaviour {
+    
+    float screenAspectRatio = 0;
+
+    void Start() {
+        screenAspectRatio = (float)Screen.width / (float)Screen.height;
+        float size = (float)(6 - (screenAspectRatio - 0.5) * 11.66);
+        if(size <= 5.3f) {
+            Camera.main.fieldOfView  = 65;
+        }else {
+            Camera.main.fieldOfView  = 65 + size;
+        }
+    }
+}
